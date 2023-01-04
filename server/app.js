33c92c4c5d0d91a98ad0c99edc494bd143eb551b
Di-Doan <92972404+Di-Doan@users.noreply.github.com>
2023-01-04 15:54:53 +0700

    up

--- server/app.js
+++ server/app.js
@@ -31,6 +31,12 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 // routes
 app.use("/billboards", billboardRouter);
 app.use("/users", userRouter);
